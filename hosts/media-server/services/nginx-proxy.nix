7355698b3252e78eb7e18e745493745e64364393
Nico Swan <nico@bcbgroup.io>
2024-08-16 13:22:15 +0200

    update nginx

--- hosts/media-server/services/nginx-proxy.nix
+++ hosts/media-server/services/nginx-proxy.nix
@@ -16,27 +16,28 @@
     in
     {
       "plex.home.nicoswan.com" = (SSL // {
-        locations."/".proxyPass = "http://127.0.0.1:32400";
+        http2 = true;
+        locations."/".proxyPass = "http://127.0.0.1:32400/";
       });
 
       "radarr.home.nicoswan.com" = (SSL // {
-        locations."/".proxyPass = "http://127.0.0.1:7878";
+        locations."/".proxyPass = "http://127.0.0.1:7878/";
       });
 
       "sonarr.home.nicoswan.com" = (SSL // {
-        locations."/".proxyPass = "http://127.0.0.1:8989";
+        locations."/".proxyPass = "http://127.0.0.1:8989/";
       });
 
       "ombi.home.nicoswan.com" = (SSL // {
-        locations."/".proxyPass = "http://127.0.0.1:5000";
+        locations."/".proxyPass = "http://127.0.0.1:5000/";
       });
 
       "qbittorrent.home.nicoswan.com" = (SSL // {
-        locations."/".proxyPass = "http://127.0.0.1:9010";
+        locations."/".proxyPass = "http://127.0.0.1:9010/";
       });
       
       "jackett.home.nicoswan.com" = (SSL // {
-        locations."/".proxyPass = "http://127.0.0.1:9117";
+        locations."/".proxyPass = "http://127.0.0.1:9117/";
       });
 
     };
