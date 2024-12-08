dc5a43e613a23e01cee9016f90099ac9d75474f8
Nico Swan <hi@nicoswan.com>
2024-12-08 07:18:52 +0200

    updated keycloak

--- hosts/vm403bfeq/services/keycloak/default.nix
+++ hosts/vm403bfeq/services/keycloak/default.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 let
   username = "$(cat ${config.sops.secrets."servers/cygnus-labs/keycloak/dbUsername".path})";
   passwordFile = config.sops.secrets."servers/cygnus-labs/keycloak/dbPassword".path;
@@ -6,42 +6,50 @@ in
 {
 
   sops = {
-    secrets = {
-      "servers/cygnus-labs/keycloak/dbUsername" = { };
-      "servers/cygnus-labs/keycloak/dbPassword" = { };
-    };
+     secrets = {
+        "servers/cygnus-labs/keycloak/dbUsername" = {};
+        "servers/cygnus-labs/keycloak/dbPassword" = {};
+     };
   };
+
+  environment.systemPackages = with pkgs; [
+    keycloak
+  ];
 
   services.nginx = {
     virtualHosts = {
       "keycloak.cygnus-labs.com" = {
-        #useACMEHost = "cygnus-labs.com";
-        enableACME = true;
+        useACMEHost = "cygnus-labs.com";
+        #enableACME = true;
         forceSSL = true;
-        locations."/".proxyPass = "http://127.0.0.1:38080";
+        locations."/".proxyPass = "http://localhost:38080";
       };
     };
   };
 
   services.keycloak = {
     enable = true;
-
     database = {
       type = "postgresql";
       createLocally = true;
-
       username = "keycloak";
       passwordFile = "${passwordFile}";
     };
 
+    initialAdminPassword = "changeme";
     settings = {
-      hostname = "keycloak.cygnus-labs.com";
       http-enabled = true;
       hostname-strict-https = true;
       https-port = 38081;
       http-relative-path = "/";
       http-port = 38080;
-      #proxy = "passthrough";
+      hostname = "https://keycloak.cygnus-labs.com";
+      http-host = "127.0.0.1";
+    # Uncomment the following line if you need to add CSP headers directly to Keycloak
+      securityHeaders = "frame-src 'self' http://keycloak.cygnus-labs.com";
+
     };
+
   };
 }
+
