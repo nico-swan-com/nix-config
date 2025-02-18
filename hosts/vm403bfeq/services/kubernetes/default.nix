4c53f750bddd914c90c4a42fc53a86582de447f4
Nico Swan <hi@nicoswan.com>
2025-02-18 11:01:36 +0200

    added jobs to prevent Diskpresure on kubernetes with unused images

--- hosts/vm403bfeq/services/kubernetes/default.nix
+++ hosts/vm403bfeq/services/kubernetes/default.nix
@@ -1,19 +1,18 @@
 { config, pkgs, ... }:
 let
-  my-kubernetes-helm = with pkgs; wrapHelm kubernetes-helm {
-    plugins = with pkgs.kubernetes-helmPlugins; [
-      helm-secrets
-      helm-diff
-      helm-s3
-      helm-git
-    ];
-  };
+  my-kubernetes-helm = with pkgs;
+    wrapHelm kubernetes-helm {
+      plugins = with pkgs.kubernetes-helmPlugins; [
+        helm-secrets
+        helm-diff
+        helm-s3
+        helm-git
+      ];
+    };
 
-  my-helmfile = pkgs.helmfile-wrapped.override {
-    inherit (my-kubernetes-helm) pluginsDir;
-  };
-in
-{
+  my-helmfile =
+    pkgs.helmfile-wrapped.override { inherit (my-kubernetes-helm) pluginsDir; };
+in {
   imports = [
     #./certificates.nix
     ./cluster.nix
@@ -30,5 +29,42 @@ in
     my-kubernetes-helm
     my-helmfile
   ];
+
+  # This is to prevent the DiskPresure to occure because the diskspace get full because of unused images 
+  systemd.services = {
+    docker-prune = {
+      description = "Prune Docker images";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.docker}/bin/docker image prune -a -f";
+      };
+    };
+    containerd-prune = {
+      description = "Prune Containerd images";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.containerd}/bin/ctr -n k8s.io images prune --all";
+      };
+    };
+  };
+
+  systemd.timers = {
+    docker-prune = {
+      description = "Run Docker prune weekly";
+      timerConfig = {
+        OnCalendar = "weekly";
+        Persistent = true;
+      };
+      wantedBy = [ "timers.target" ];
+    };
+    containerd-prune = {
+      description = "Run Containerd prune weekly";
+      timerConfig = {
+        OnCalendar = "weekly";
+        Persistent = true;
+      };
+      wantedBy = [ "timers.target" ];
+    };
+  };
 
 }
