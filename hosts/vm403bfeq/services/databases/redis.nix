06f289ab3e45c42e34d58d5b3c937fd7f394628b
Nico Swan <hi@nicoswan.com>
2025-02-22 08:47:09 +0200

    updated redis

--- hosts/vm403bfeq/services/databases/redis.nix
+++ hosts/vm403bfeq/services/databases/redis.nix
@@ -1,10 +1,27 @@
-{
+{ config, ... }: {
+
+  sops = { secrets = { "servers/cygnus-labs/redis/requirePassFile" = { }; }; };
+
   services.redis = {
-    servers."cygnus-labs" = {
-      enable = true;
-      #user = "nextcloud";
-      #unixSocket = "/run/redis-nextcloud/redis.sock";
-      port = 6379;
+    servers = {
+      "cygnus-labs" = {
+        enable = true;
+        openFirewall = true;
+        requirePassFile =
+          config.sops.secrets."servers/cygnus-labs/redis/requirePassFile".path;
+        port = 6379;
+        bind = null;
+      };
+      "penpot" = {
+        enable = true;
+        openFirewall = true;
+        requirePassFile =
+          config.sops.secrets."servers/cygnus-labs/redis/requirePassFile".path;
+        #user = "nextcloud";
+        #unixSocket = "/run/redis-nextcloud/redis.sock";
+        port = 6380;
+        bind = null;
+      };
     };
   };
 }
