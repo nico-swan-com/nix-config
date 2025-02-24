e81c50f22abd539703960ebcb24d295e4ea4a02e
Nico Swan <hi@nicoswan.com>
2025-02-24 22:27:10 +0200

    test service

--- hosts/vm403bfeq/services/test-service.nix
+++ hosts/vm403bfeq/services/test-service.nix
@@ -1,5 +1,4 @@
 {
-
   services.nginx = {
     enable = true;
     recommendedGzipSettings = true;
@@ -11,7 +10,7 @@
     virtualHosts = {
       "test.cygnus-labs.com" = {
         useACMEHost = "cygnus-labs.com";
-        #enableACME = true;
+        # enableACME = true;
         forceSSL = true;
 
         locations = {
@@ -26,13 +25,26 @@
           };
           "/auth-keycloak" = {
             proxyPass =
-              "https://keycloak.cygnus-labs.com/realms/production/account/";
+              "https://keycloak.cygnus-labs.com/realms/production/protocol/openid-connect/token/introspect";
             extraConfig = ''
               internal;
               proxy_ssl_server_name on;
               proxy_pass_request_body off;
               proxy_set_header Content-Length "";
               proxy_set_header X-Original-URI $request_uri;
+              proxy_set_header Authorization "Bearer $http_authorization";
+              proxy_set_header X-Client-IP $remote_addr;
+              proxy_set_header X-Client-Proto $scheme;
+              proxy_set_header Host $host;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+            '';
+          };
+          "@handle_unauthorized" = {
+            return = 302;
+            extraConfig = ''
+              return 302 https://keycloak.cygnus-labs.com/realms/production/protocol/openid-connect/auth?client_id=your_client_id&redirect_uri=$scheme://$http_host$request_uri&response_type=code&scope=openid;
             '';
           };
         };
@@ -46,5 +58,4 @@
       ports = [ "8080:8080" "8081:8081" ];
     };
   };
-
 }
