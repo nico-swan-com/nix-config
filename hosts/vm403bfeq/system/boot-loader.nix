1d5d291d8aa509dbdc01d0b4e2f00a4122bb4404
Nico Swan <hi@nicoswan.com>
2025-03-18 19:30:26 +0200

    update vps bootloader

--- hosts/vm403bfeq/system/boot-loader.nix
+++ hosts/vm403bfeq/system/boot-loader.nix
@@ -2,7 +2,10 @@
   # Bootloader.
   boot.loader.grub = {
     enable = true;
-    device = "/dev/sda";
+    device = "nodev";
+    efiSupport = true;
     configurationLimit = 3;
+    copyKernels = false;
+    fsIdentifier = "label";
   };
 }
