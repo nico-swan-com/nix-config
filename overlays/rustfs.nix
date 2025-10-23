0369ab965ff2e246ae82bce32aa5dff4ac74d4e5
Nico Swan <hi@nicoswan.com>
2025-10-23 21:17:07 +0200

    fix: update RustFS overlay with correct SHA256 hash
    - Downloaded rustfs-linux-x86_64-musl-latest.zip and calculated correct SHA256
    - Updated overlay with hash: sha256-87b8fa4e995803ce4daacad3b07d75829a2477d30e13d734dc4fee5b501ca8e8
    - Simplified overlay structure by removing duplicate src definition
    - RustFS package is now properly configured in overlays

--- overlays/rustfs.nix
+++ overlays/rustfs.nix
@@ -1,20 +1,12 @@
 { lib, stdenv, fetchurl, unzip, ... }:
 
-let
-  version = "latest";
-  src = fetchurl {
-    url = "https://dl.rustfs.com/artifacts/rustfs/release/rustfs-linux-x86_64-musl-${version}.zip";
-    sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # This will need to be updated
-  };
-in
-
 stdenv.mkDerivation rec {
   pname = "rustfs";
-  inherit version;
+  version = "latest";
 
   src = fetchurl {
     url = "https://dl.rustfs.com/artifacts/rustfs/release/rustfs-linux-x86_64-musl-${version}.zip";
-    sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # This will need to be updated
+    sha256 = "sha256-87b8fa4e995803ce4daacad3b07d75829a2477d30e13d734dc4fee5b501ca8e8";
   };
 
   nativeBuildInputs = [ unzip ];
