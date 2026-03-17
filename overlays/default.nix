d77630ad0471046e4adfe1686868c331902c1fcc
Nico Swan <hi@nicoswan.com>
2026-03-17 06:27:58 +0200

    chore: Update NUR dependency and refactor Nixpkgs imports to use `stdenv.hostPlatform.system`.

--- overlays/default.nix
+++ overlays/default.nix
@@ -19,13 +19,13 @@
   # be accessible through 'pkgs.unstable'
   unstable-packages = final: _prev: {
     unstable = import inputs.nixpkgs-unstable {
-      system = final.system;
+      system = final.stdenv.hostPlatform.system;
       config.allowUnfree = true;
     };
   };
   stable-packages = final: _prev: {
     stable = import inputs.nixpkgs-stable {
-      system = final.system;
+      system = final.stdenv.hostPlatform.system;
       config.allowUnfree = true;
     };
   };
